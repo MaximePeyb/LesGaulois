16d1c6909b4c3b4a67885c5ff25ff6e0f947ca7e
Maxime Peybernes <maxime.peybernes@gmail.com>
2024-09-23 19:32:45 +0200

    TP2 terminé - les énumérés

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,6 +3,8 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement equipement[] = new Equipement[2];
+	private int nbEquipement = 0;
 
 	public Romain(String nom, int force) {
 		this.nom = nom;
@@ -36,10 +38,34 @@ public class Romain {
 		assert force<forceAvant;
 	}
 	
+	public void sEquiper(Equipement e) {
+		switch (nbEquipement){
+			case 2:
+				System.out.println("Le soldat Minus est déjà bien protégé !");
+				break;
+			case 1:
+				if(e==equipement[0]) {
+					parler("Je possède déjà un "+e+" !");
+					break;
+				} 
+			case 0:
+				equipement[nbEquipement++] = e;
+				parler("Je m'équipe avec un "+e+".");
+				break;
+			default:
+				System.out.println("Problème... switch sEquiper Romain");
+		}
+	}
+	
 	public static void main(String[] args) {
 		Romain testus = new Romain("Minus",6);
 		testus.parler("Je peux parler !!");
 		testus.recevoirCoup(2);
 		testus.recevoirCoup(2);
+		
+		testus.sEquiper(Equipement.CASQUE);
+		testus.sEquiper(Equipement.CASQUE);
+		testus.sEquiper(Equipement.BOUCLIER);
+		testus.sEquiper(Equipement.CASQUE);
 	}
 }
